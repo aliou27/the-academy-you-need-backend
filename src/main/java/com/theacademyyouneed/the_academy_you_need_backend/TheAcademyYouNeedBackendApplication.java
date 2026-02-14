02e5b41ae1a96d43cb55703ee1a6166983b3c750
aliou27 <baaliou021@gmail.com>
2026-02-14 22:46:20 +0000

    Initial commit

---
+++ src/main/java/com/theacademyyouneed/the_academy_you_need_backend/TheAcademyYouNeedBackendApplication.java
@@ -0,0 +1,13 @@
+package com.theacademyyouneed.the_academy_you_need_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheAcademyYouNeedBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheAcademyYouNeedBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/theacademyyouneed/the_academy_you_need_backend/TheAcademyYouNeedBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.theacademyyouneed.the_academy_you_need_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheAcademyYouNeedBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
